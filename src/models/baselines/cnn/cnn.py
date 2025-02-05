d22261d923bc6803b2443d4afb924c71d55b83e6
Way-Yuhao <36289795+Way-Yuhao@users.noreply.github.com>
2025-02-04 21:55:09 -0600

    cnn runs ok

--- src/models/baselines/cnn/cnn.py
+++ src/models/baselines/cnn/cnn.py
@@ -2,6 +2,56 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class SEVIRUNet(nn.Module):
+
+    def __init__(self, in_channels: int, out_channels: int, start_filters: int = 32):
+        super(SEVIRUNet, self).__init__()
+        self.start_filters = start_filters
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        # Encoder Blocks
+        self.encoder0 = EncoderBlock(self.in_channels, start_filters)
+        self.encoder1 = EncoderBlock(start_filters, start_filters * 2)
+        self.encoder2 = EncoderBlock(start_filters * 2, start_filters * 4)
+        self.encoder3 = EncoderBlock(start_filters * 4, start_filters * 8)
+        # Center Convolution Block
+        self.center = ConvBlock(start_filters * 8, start_filters * 16)
+        # Decoder Blocks
+        # self.decoder3 = DecoderBlock(start_filters * 16, start_filters * 8, start_filters * 8)
+        # self.decoder2 = DecoderBlock(start_filters * 8, start_filters * 4, start_filters * 4)
+        # self.decoder1 = DecoderBlock(start_filters * 4, start_filters * 2, start_filters * 2)
+        # self.decoder0 = DecoderBlock(start_filters * 2, start_filters, start_filters)
+        # # Final Convolution to map to output channels
+        # self.final_conv = nn.Conv2d(start_filters, 1, kernel_size=1)
+
+        # Decoder Blocks
+        self.decoder3 = DecoderBlock(start_filters * 16, start_filters * 4, start_filters * 8)
+        self.decoder2 = DecoderBlock(start_filters * 8, start_filters * 2, start_filters * 4)
+        self.decoder1 = DecoderBlock(start_filters * 4, start_filters, start_filters * 2)
+        self.decoder0 = DecoderBlock(start_filters * 2, in_channels, start_filters)
+        # Final Convolution to map to output channels
+        self.final_conv = nn.Conv2d(start_filters, self.out_channels, kernel_size=1)
+        return
+
+    def forward(self, x):
+        # Encoder Path
+        encoder0_pool, encoder0 = self.encoder0(x)
+        encoder1_pool, encoder1 = self.encoder1(encoder0_pool)
+        encoder2_pool, encoder2 = self.encoder2(encoder1_pool)
+        encoder3_pool, encoder3 = self.encoder3(encoder2_pool)
+        # Center block
+        center = self.center(encoder3_pool)
+        # Decoder Path
+        decoder3 = self.decoder3(center, encoder3)
+        decoder2 = self.decoder2(decoder3, encoder2)
+        decoder1 = self.decoder1(decoder2, encoder1)
+        decoder0 = self.decoder0(decoder1, encoder0)
+        # Final output layer
+        output = self.final_conv(decoder0)
+        return output
+
+########### INDIVIDUAL BLOCKS ############
 
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, activation=nn.ReLU()):
@@ -39,6 +89,9 @@ class EncoderBlock(nn.Module):
 
 
 class DecoderBlock(nn.Module):
+    """
+    NOTE: implementation is modified to allow for different input sizes
+    """
     def __init__(self, in_channels, concat_channels, out_channels, activation=nn.ReLU()):
         super(DecoderBlock, self).__init__()
         self.upconv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
@@ -49,52 +102,3 @@ class DecoderBlock(nn.Module):
         x = torch.cat([x, concat_tensor], dim=1)
         x = self.conv_block(x)
         return x
-
-
-class SEVIRUNet(nn.Module):
-    def __init__(self, start_filters=32):
-        super(SEVIRUNet, self).__init__()
-        self.start_filters = start_filters
-        # Encoder Blocks
-        self.encoder0 = EncoderBlock(8, start_filters)
-        self.encoder1 = EncoderBlock(start_filters, start_filters * 2)
-        self.encoder2 = EncoderBlock(start_filters * 2, start_filters * 4)
-        self.encoder3 = EncoderBlock(start_filters * 4, start_filters * 8)
-        # Center Convolution Block
-        self.center = ConvBlock(start_filters * 8, start_filters * 16)
-        # Decoder Blocks
-        # self.decoder3 = DecoderBlock(start_filters * 16, start_filters * 8, start_filters * 8)
-        # self.decoder2 = DecoderBlock(start_filters * 8, start_filters * 4, start_filters * 4)
-        # self.decoder1 = DecoderBlock(start_filters * 4, start_filters * 2, start_filters * 2)
-        # self.decoder0 = DecoderBlock(start_filters * 2, start_filters, start_filters)
-        # # Final Convolution to map to output channels
-        # self.final_conv = nn.Conv2d(start_filters, 1, kernel_size=1)
-
-        # Decoder Blocks
-        self.decoder3 = DecoderBlock(start_filters * 16, start_filters * 4, start_filters * 8)
-        self.decoder2 = DecoderBlock(start_filters * 8, start_filters * 2, start_filters * 4)
-        self.decoder1 = DecoderBlock(start_filters * 4, start_filters, start_filters * 2)
-        self.decoder0 = DecoderBlock(start_filters * 2, 8, start_filters)
-        # Final Convolution to map to output channels
-        self.final_conv = nn.Conv2d(start_filters, 1, kernel_size=1)
-
-    def forward(self, x):
-        # Encoder Path
-        encoder0_pool, encoder0 = self.encoder0(x)
-        encoder1_pool, encoder1 = self.encoder1(encoder0_pool)
-        encoder2_pool, encoder2 = self.encoder2(encoder1_pool)
-        encoder3_pool, encoder3 = self.encoder3(encoder2_pool)
-
-        # Center block
-        center = self.center(encoder3_pool)
-
-        # Decoder Path
-        decoder3 = self.decoder3(center, encoder3)
-        decoder2 = self.decoder2(decoder3, encoder2)
-        decoder1 = self.decoder1(decoder2, encoder1)
-        decoder0 = self.decoder0(decoder1, encoder0)
-
-        # Final output layer
-        output = self.final_conv(decoder0)
-        return output
-
--- src/models/baselines/cnn/cnn_module.py
+++ src/models/baselines/cnn/cnn_module.py
@@ -1,4 +1,78 @@
 from typing import Any, Dict, Tuple, Optional
 import torch
-from lightning import LightningModule
+from src.models.event.generic_e2d_module import GenericE2DModule
+from src.utils.event.event_data_utils import resize_to_multiple_of_16
 from src.utils.helper import yprint
+
+
+class CNNLitModule(GenericE2DModule):
+    """
+    A PyTorch Lightning module for a simple CNN.
+    Serves to provide a deterministic baseline for comparison.
+    Inherits from `GenericE2DModule` which provides a template for event-to-depth models.
+    """
+
+    def __init__(self, net: torch.nn.Module, optimizer: torch.optim.Optimizer,
+                 criterion, compile: bool = False,
+                 *args, **kwargs) -> None:
+        super().__init__()
+        self.net = net
+        self.criterion = criterion
+        self.save_hyperparameters(logger=False, ignore=("net", "optimizer_config", "criterion"))
+        return
+
+    def setup(self, stage: str) -> None:
+        if self.hparams.compile and stage == "fit":
+            self.net = torch.compile(self.net)
+            print("Model compiled.")
+        elif self.hparams.compile and stage == "test":
+            print("Warning: torch_compile is only available during the fit stage.")
+        return
+
+    def configure_optimizers(self) -> Dict[str, Any]:
+        optimizer = self.hparams.optimizer(params=self.trainer.model.parameters())
+        return {"optimizer": optimizer}
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.net(x)
+
+    def training_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> Dict[str, torch.Tensor]:
+        """Perform a single training step on a batch of data from the training set.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target
+            labels.
+        :param batch_idx: The index of the current batch.
+        :return: A tensor of losses between model predictions and targets.
+        """
+        condition, gt = self._generate_condition(batch)
+        condition = resize_to_multiple_of_16(condition)
+        gt = resize_to_multiple_of_16(gt)
+        prediction = self.forward(condition)
+        loss = self.criterion(prediction, gt)
+        self.log("train/loss", loss, on_step=True, on_epoch=False, prog_bar=False, batch_size=condition.shape[0])
+        # step_output = {"batch_dict": batch, "loss_dict": loss_dict, 'condition': condition, 'context_mask': context_mask}
+        return loss
+
+    def validation_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> Dict[str, torch.Tensor]:
+        """
+        Perform a single validation step on a batch of data from the validation set.
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target labels.
+        :param batch_idx: The index of the current batch.
+        """
+        batch = self._fill_missing_keys(batch)
+        condition, gt = self._generate_condition(batch)
+        condition = resize_to_multiple_of_16(condition)
+        gt = resize_to_multiple_of_16(gt)
+        prediction = self.forward(condition)
+        eval_loss = self.criterion(prediction, gt)
+        self.log("val/loss", eval_loss, on_step=False, on_epoch=True, prog_bar=False,
+                 batch_size=condition.shape[0], sync_dist=True)
+        step_output = {"batch_dict": batch, "condition": condition}
+        return step_output
+
+    def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> Dict[str, torch.Tensor]:
+        raise NotImplementedError()
+
+    def sample(self, condition: torch.Tensor) -> torch.Tensor:
+        return self.forward(condition)
+
